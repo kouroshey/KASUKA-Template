a0d68e935b0df1e5a01d79ea6f503167462c8c9f
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2023-06-28 12:04:03 +0330

    fix slider problems

--- src/Compponents/Portolio/SlideShow.js
+++ src/Compponents/Portolio/SlideShow.js
@@ -8,8 +8,13 @@ import slide4 from './slides/slide-4.png';
 
 export default function SlideShow() {
     const [currentSlide, setCurrentSlide] = useState(0)
-    const slides = [slide1 , slide2 , slide3 , slide4]
-    
+    const slides = [slide1, slide2, slide3, slide4];
+
+    const paginationDots = slides.map((slide, index) => (
+        <span className='dot'></span>
+    ))
+    console.log(paginationDots);
+
     const nextSlide = () => {
         if (currentSlide === slides.length - 1) {
             setCurrentSlide(0)
@@ -17,7 +22,7 @@ export default function SlideShow() {
             setCurrentSlide(currentSlide + 1)
         }
         console.log(currentSlide);
-        
+
     }
 
     const prevSlide = () => {
@@ -37,14 +42,24 @@ export default function SlideShow() {
 
                 {/* slides */}
                 <div className="slide">
-                    {slides.map((slide , index)=> (
+                    {slides.map((slide, index) => (
                         <img src={slide} className={index === currentSlide ? "slide-img show" : "slide-img"} />
-                        
+
                     ))}
-                {/* arrows */}
-                <i class='bx bx-chevron-left prev' onClick={prevSlide}></i>
-                <i class='bx bx-chevron-right next' onClick={nextSlide} ></i>
-                {/* end of arrows */}
+                    {/* arrows */}
+                    <i class='bx bx-chevron-left prev' onClick={nextSlide}></i>
+                    <i class='bx bx-chevron-right next' onClick={prevSlide} ></i>
+                    {/* end of arrows */}
+
+                    {/* dots section */}
+
+                    <div className="dots-section">
+                        <div className="dots">
+                            {slides.map((slide, index) => (
+                                <span className={`dot ${index === currentSlide ? 'dot-active' : ''}`}></span>
+                            )) }
+                        </div>
+                    </div>
                 </div>
                 {/*end of slides */}
 
